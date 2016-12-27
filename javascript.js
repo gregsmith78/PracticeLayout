3b7943161a7b1d7576ac30a1a997df89971d74b6
Greg Smith <greg@flipeleven.com>
2016-12-27 17:04:49 -0600

    trying to incorporate JS functionality

---
+++ javascript.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var _dsaBox = document.getElementById(".dsaBox");
+
+$(document).ready(function() {
+    $('.dsaBox').click(function() {
+        alert('test');
+    })
+});
+
+/*
+(function _init(){
+	alert("Test!!");
+})();
+*/
+function testClick(){
+	
+
+}
+
+function _animateIn(){
+
+switch(e) {
+	case 'over':
+		_over();
+	break;
+	case 'out':
+		_out();
+	break;
+
+}
+}
+
+function _over(element){
+
+}
+
+function _out (element) {
+	// body...
+}
