bb9c0189dad587f0bc044cc6ecbfe33aafcc0815
Greg Smith <greg@flipeleven.com>
2017-01-03 11:25:34 -0600

    created gruntfile

---
+++ javascript.js
@@ -0,0 +1,30 @@
+'use strict';
+//import com.greensock.*;
+//import com.greensock.easing.*;
+
+
+
+//var _dsaBox = document.getElementById(".dsaBox");
+
+function moveBox(element){
+TweenMax.to(".dsaBox", 1, {y:-100, delay: 2});
+}
+////
+//var numTxt = new SplitText(".numBox p", {type:"chars"}),
+//	numBxChars = numTxt.chars.length,
+//	tl = new TimelineMax({delay:0.5, repeat:-1});
+//
+//TweenLite.set(".numBox", {visibility:"visible"});
+//tl.staggerTo(numTxt.chars, 0.8, {ease:Back.easeOut, cycle:{y:[100, -100], rotation:[-120, 120]}, opacity: 0,}, 0.03, "+=1");
+
+//$(document).ready(function() {
+//    $('.dsaBox').click(function() {
+//        tween.('dasBox');
+//  })
+//});
+
+
+//(function _init(){
+//	moveBox(".dsaBox");
+//})();
+
